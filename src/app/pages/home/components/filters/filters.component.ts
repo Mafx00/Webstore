e9ff7733204eada8648767a83ba2dc2496f89508
Mafalda Nunes <gogreen.m@hotmail.com>
2023-07-04 15:41:05 +0200

    home angular

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+//import { StoreService } from 'src/app/services/store.service';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+})
+export class FiltersComponent implements OnInit, OnDestroy {
+  @Output() showCategory = new EventEmitter<string>();
+  categories: string[] | undefined;
+  categoriesSubscription: Subscription | undefined;
+
+  //constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+
+  }
+
+  onShowCategory(category: string): void {
+    this.showCategory.next(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
+  }
+}
